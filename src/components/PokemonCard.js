f3f651b75325661154a6aaf9f26448c812bc09c5
mariobalrod <marioballestero.rdg@gmail.com>
2020-07-19 11:37:11 +0200

    refactor : some fixes at Pokemon Cards with types

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -5,21 +5,46 @@ import PokeballCard from '../svg/pokeballCard.svg';
 
 export default function PokemonCard (props) {
 
+    const [ typesNames, setTypesNames ] = React.useState([]);
+
+    React.useEffect(() => {
+        setNames(props.data.types);
+    }, [props.data]);
+
+    const setNames = (types) => {
+        const temp = []; 
+        console.log('Array', types)
+        for (let i = 0; i<types.length; i++) {
+            const name = types[i].type.name;
+            temp.push(name);
+        }
+        setTypesNames(temp);
+    }
+
+    const { id, name, sprites } = props.data;
+
     return(
         <div className="card">
             <div className="foreground">
                 <div className="section1">
-                    <h1 className="name">{props.data.name}</h1>
-                    <h2 className="number">{'#'+props.data.id.toString().padStart(3, "0")}</h2>
+                    <h1 className="name">{name}</h1>
+                    <h2 className="number">
+                        {'#'+id.toString().padStart(3, "0")}
+                    </h2>
                 </div>
                 <div className="section2">
-                    <img className="image" src={props.data.sprites.front_default} alt="pokemon" />
+                    <img className="image" src={sprites.front_default} alt="pokemon" />
                     <div className="subsection">
-                        <div className="chip">
                             {
-                                props.data.types.map((type, i) => <h4 key={i} className="chipText">{type.type.name}</h4>)
+                                typesNames.map((name, i) => {
+                                    return (
+                                        <div className="chip">
+                                            <h4 key={i} className="chipText">{name}</h4>
+                                        </div>
+                                    );
+                                })
+                            
                             }
-                        </div>
                     </div>
                 </div>
             </div>
